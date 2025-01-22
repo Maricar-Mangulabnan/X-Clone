ce7251921c927a3cf791331727b51c259b64e2e2
Maricar-Mangulabnan <mangulabnanrica3@gmail.com>
2025-01-23 00:58:28 +0800

    email and pass

--- js/includes.js
+++
@@ -1,8 +0,0 @@
-function loadPartial(elementId, url) {
-    fetch(url)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById(elementId).innerHTML = data;
-      })
-      .catch(error => console.error('Error fetching partial:', error));
-  }
